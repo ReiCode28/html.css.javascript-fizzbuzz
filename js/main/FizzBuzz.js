525f992066568a5147439bd4198b816b2e217cb1
randy <randyr_1993@yahoo.com>
2023-01-04 12:53:57 -0500

    Completed

--- js/main/FizzBuzz.js
+++ js/main/FizzBuzz.js
@@ -1,23 +1,41 @@
+/**
+ * If the number is divisible by 3, print "Fizz".
+ * If the number is divisible by 5, print "Buzz".
+ * If the number is divisible by both 3 and 5, print "FizzBuzz".
+ */
+
 class FizzBuzz {
     constructor(fizz, buzz) {
       this.fizz = fizz;
       this.buzz = buzz;
     }
-  
+    
     isFizz(num) {
-        return null;
+        return num % this.fizz === 0;
+        //returns `true` if the given number is divisible by the `fizz` property of the object.
     }
-  
+    
     isBuzz(num) {
-        return null;
+        return num % this.buzz === 0;
+        //returns `true` if the given number is divisible by the `buzz` property of the object.
     }
-  
+    
     isFizzBuzz(num) {
-        return null;
+        return this.isFizz(num) && this.isBuzz(num);
+        //returns `true` if the given number is divisible by both the `fizz` and `buzz` properties of the object.
     }
   
     getOutput(num) {
-        return null;
+        if (this.isFizzBuzz(num)){
+            return 'FizzBuzz';
+        } else if (this.isFizz(num)){
+            return 'Fizz';
+        } else if (this.isBuzz(num)){
+            return 'Buzz'
+        } else
+        return num;
+
+        //returns the appropriate string for the given number, according to the Fizz Buzz rules
     }
   }
   
